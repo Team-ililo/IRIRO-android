80efa8a57d606b518f9aa59545793c4c18d55911
SongKong00 <hesong1@naver.com>
2023-04-05 11:35:56 +0900

    Feat: 프로젝트 오류로 인한 기존 내용 재생성

---
+++ app/src/main/java/com/example/ililo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ililo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ililo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.containerFragment.id, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.navBottom.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menu_home -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, HomeFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_park -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, ParkFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_mypage -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, MyPageFragment())
+                            .commitAllowingStateLoss()
+                    }
+                }
+                true
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ililo/MyPageFragment.kt
@@ -0,0 +1,18 @@
+package com.example.ililo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ililo.databinding.FragmentMypageBinding
+
+class MyPageFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentMypageBinding.inflate(layoutInflater).root
+    }
+}
