14f77324db037590d512f45368dbb98a4b37d1df
SongKong00 <hesong1@naver.com>
2023-04-06 15:25:23 +0900

    Design: 디자인 수정

--- app/src/main/java/com/example/ililo/HomeFragment.kt
+++ app/src/main/java/com/example/ililo/HomeFragment.kt
@@ -1,18 +1,44 @@
 package com.example.ililo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.ililo.databinding.FragmentHomeBinding
+import com.example.ililo.databinding.FragmentMypageBinding
 
 class HomeFragment: Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return FragmentHomeBinding.inflate(layoutInflater).root
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //공지사항 이동
+        binding.tvNotice.setOnClickListener {
+            val intent = Intent(context, Notice::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnGoNotice.setOnClickListener {
+            val intent = Intent(context, Notice::class.java)
+            startActivity(intent)
+        }
+
+        binding.layoutNotice.setOnClickListener {
+            val intent = Intent(context, Notice::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/ililo/MyPageFragment.kt
+++ app/src/main/java/com/example/ililo/MyPageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.ililo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +9,30 @@ import android.view.ViewGroup
 import com.example.ililo.databinding.FragmentMypageBinding
 
 class MyPageFragment: Fragment() {
+    private var _binding: FragmentMypageBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return FragmentMypageBinding.inflate(layoutInflater).root
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //공지사항 이동
+        binding.tvNotice.setOnClickListener {
+            val intent = Intent(context, Notice::class.java)
+            startActivity(intent)
+        }
+
+        //신고하기 이동
+        binding.tvGoWarn.setOnClickListener {
+            val intent = Intent(context, Declare::class.java)
+            startActivity(intent)
+        }
     }
 }
