243e46873201e3eb80f56ffc54714dd2ea6a0402
SongKong00 <hesong1@naver.com>
2023-04-06 11:45:04 +0900

    Design: 공지사항 페이지 생성

---
+++ app/src/main/java/com/example/ililo/Notice.kt
@@ -0,0 +1,16 @@
+package com.example.ililo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ililo.databinding.ActivityNoticeBinding
+
+class Notice : AppCompatActivity() {
+    private val binding: ActivityNoticeBinding by lazy {
+        ActivityNoticeBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
