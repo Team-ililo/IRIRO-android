5352f9186dcc8ad280ee2ab0326783c0b601b4b1
SongKong00 <hesong1@naver.com>
2023-05-10 03:20:18 +0900

    Feat: 출차 시간 등록 다이얼로그 생성 및 리사이클러뷰 적용

--- app/src/main/java/com/example/ililo/Home/HomeFragment.kt
+++ app/src/main/java/com/example/ililo/Home/HomeFragment.kt
@@ -1,14 +1,24 @@
 package com.example.ililo.Home
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.example.ililo.Declare.MemberDeclareListActivity
-import com.example.ililo.NoticeActivity
+import com.example.ililo.Notice.NoticeActivity
+import com.example.ililo.R
 import com.example.ililo.databinding.FragmentHomeBinding
+
 
 class HomeFragment: Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -22,6 +32,7 @@ class HomeFragment: Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -47,5 +58,49 @@ class HomeFragment: Fragment() {
             startActivity(intent)
         }
 
+        //출차시간 등록
+        binding.layoutRegister.setOnClickListener {
+            registerTime()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun registerTime(){
+
+        val mDialogView = LayoutInflater.from(context).inflate(R.layout.dialog_timepicker, null)
+        val mBuilder = android.app.AlertDialog.Builder(context).setView(mDialogView)
+        val mAlertDialog = mBuilder.show()
+
+        val tPicker = mDialogView.findViewById<TimePicker>(R.id.timePicker)
+        val cBox = mDialogView.findViewById<CheckBox>(R.id.checkBox)
+        val save = mDialogView.findViewById<TextView>(R.id.btn_save)
+        val cancel = mDialogView.findViewById<TextView>(R.id.btn_cancel)
+
+        mAlertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))  //배경 투명처리 해줘야 배경 모양 드러남
+
+        save.setOnClickListener{
+            if(cBox.isChecked){
+                binding.tvRegisterTime.text = "장시간 이동 예정이 없어요!"
+                binding.tvGoOutTime.text = "장기 주차 예정"
+                mAlertDialog.dismiss()
+            }
+            else{
+                var hour = (tPicker.hour.toString().padStart(2, '0')).toInt()
+                val min = tPicker.minute.toString().padStart(2, '0')
+                if (hour > 12) {
+                    hour = hour - 12
+                    binding.tvRegisterTime.text = "오후 $hour 시 $min 분"
+                    binding.tvGoOutTime.text = "오후 $hour 시 $min 분"
+                } else {
+                    binding.tvRegisterTime.text = "오전 $hour 시 $min 분"
+                    binding.tvGoOutTime.text = "오전 $hour 시 $min 분"
+                }
+                mAlertDialog.dismiss()
+            }
+        }
+
+        cancel.setOnClickListener{
+            mAlertDialog.dismiss()
+        }
     }
 }
