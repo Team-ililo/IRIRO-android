c8a36f887e2129c7f862b93c21a5efcd90a43069
SongKong00 <hesong1@naver.com>
2023-05-15 23:33:21 +0900

    Feat: Retrofit ApplicationClass 구성 및 BaseResponse 구성

---
+++ app/src/main/java/com/example/ililo/ApplicationClass.kt
@@ -0,0 +1,48 @@
+package com.example.ililo
+
+import android.app.Application
+import android.content.SharedPreferences
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ApplicationClass : Application() {
+    val API_URL = "http://15.165.97.134:8080/"
+
+    companion object {
+        //프로필 정보 저장 위한 SharedPreferences
+        lateinit var prefs: SharedPreferences
+
+        // Retrofit 인스턴스, 앱 실행시 한번만 생성하여 사용
+        lateinit var sRetrofit: Retrofit
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // shared preference
+        prefs =
+            applicationContext.getSharedPreferences("prefs", MODE_PRIVATE)
+
+        // retrofit instance
+        initRetrofitInstance()
+    }
+
+    private fun initRetrofitInstance() {
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .readTimeout(5000, TimeUnit.MILLISECONDS)
+            .connectTimeout(5000, TimeUnit.MILLISECONDS)
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+
+
+        sRetrofit = Retrofit.Builder()
+            .baseUrl(API_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
+}
---
+++ app/src/main/java/com/example/ililo/BaseResponse.kt
@@ -0,0 +1,7 @@
+package com.example.ililo
+
+open class BaseResponse(
+    val status: String? = null,
+    val message: String? = null
+)
+
